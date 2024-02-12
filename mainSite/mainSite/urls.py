21660cc0af0a86fadf5e5b748b577dd99cc92aa5
杜博宇 <1537837879@163.com>
2024-02-12 11:39:46 +0800

    something modify

--- mainSite/mainSite/urls.py
+++ mainSite/mainSite/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.static import serve
 
+
 urlpatterns = [
     path("", include("corpus.urls")),
     path('admin/', admin.site.urls),
     re_path('media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path('static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}, name="static"),
 ]
